7568627b863ac0bba30a9df7152c37d4dbbf1a71
Finn Thorwarth <mail@finnthorwarth.de>
2025-08-23 12:43:42 +0200

    Menü Icon

--- DistributionPackages/Caspary.Site/Resources/Private/JavaScript/components/Navigation.vue
+++ DistributionPackages/Caspary.Site/Resources/Private/JavaScript/components/Navigation.vue
@@ -2,11 +2,52 @@
   <div class="navigation-wrapper font-headline">
     <button
       @click="toggleMenu"
-      class="flex items-center hover:bg-gray-100"
+      class="flex items-center"
       :aria-expanded="isMenuOpen"
       aria-label="Menü öffnen/schließen"
     >
-      <img :src="menuIconUrl" alt="Menü" class="h-8" />
+      <!-- Animated SVG Hamburger/X Icon with MENÜ Text -->
+      <svg class="h-8" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 246.6 56.7">
+        <defs>
+          <clipPath id="clippath">
+            <rect class="menu-icon0" x="103.7" y="1.7" width="137.6" height="44.6"/>
+          </clipPath>
+        </defs>
+        
+        <!-- Animated Hamburger/X Lines -->
+        <!-- Top line -->
+        <rect 
+          class="menu-icon3 hamburger-svg-line" 
+          :x="isMenuOpen ? '15' : '6'" 
+          :y="isMenuOpen ? '23' : '2.8'" 
+          :width="isMenuOpen ? '60' : '79.4'" 
+          :height="isMenuOpen ? '10' : '11.3'"
+          :transform="isMenuOpen ? 'rotate(45 45 28.35)' : ''"
+        />
+        <!-- Middle line -->
+        <rect 
+          class="menu-icon4 hamburger-svg-line" 
+          x="6" 
+          y="22.7" 
+          width="79.4" 
+          height="11.3"
+          :style="isMenuOpen ? 'opacity: 0;' : 'opacity: 1;'"
+        />
+        <!-- Bottom line -->
+        <rect 
+          class="menu-icon1 hamburger-svg-line" 
+          :x="isMenuOpen ? '15' : '6'" 
+          :y="isMenuOpen ? '23' : '42.5'" 
+          :width="isMenuOpen ? '60' : '79.4'" 
+          :height="isMenuOpen ? '10' : '11.3'"
+          :transform="isMenuOpen ? 'rotate(-45 45 28.35)' : ''"
+        />
+        
+        <!-- MENÜ Text -->
+        <g class="menu-icon5">
+          <path class="menu-icon2" d="M227.6,46.3c9,0,13.7-5.3,13.7-14.7V11.4h-5.5v19.9c0,6.9-2.5,9.9-8.2,9.9s-8.2-3-8.2-9.8V11.4h-5.5v20.3c0,9.6,4.4,14.6,13.7,14.6M232.5,7.7c1.7,0,3-1.3,3-3s-1.3-3-3-3-3,1.3-3,3,1.2,3,3,3M222.8,7.7c1.7,0,3-1.3,3-3s-1.3-3-3-3-3,1.3-3,3,1.2,3,3,3M181.5,45.3v-15.4c0-3.3,0-6.1-.2-9h0c2.3,2.9,4.3,5.4,6.3,7.7l14.7,17.2h3.1V11.4h-5.5v14.5c0,3.4,0,6,.2,8.9h-.1c-2.2-2.9-4.2-5.3-6.2-7.8l-13.3-15.7h-4.5v34h5.5ZM167.3,30.2v-4.5h-13.9v-9.5h15.2l-.6-4.9h-20.2v34h20.2l.6-4.9h-15.2v-10.2h13.9ZM109.1,45.3v-16.6c0-3.4,0-6.9,0-10.3h.1c1.3,2.6,2.6,5.2,4,7.9l7.8,15.4h.9l7.9-15.4,4-7.9h.1c0,3.4,0,6.9,0,10.3v16.6h5.4V11.4h-7.1l-10.6,21.2h-.1l-10.6-21.2h-7.1v34h5.4Z"/>
+        </g>
+      </svg>
     </button>
 
     <!-- Desktop Menu Overlay -->
