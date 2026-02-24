65fe0a83789d119b0dd712464ad679e004798b2a
Finn Thorwarth <mail@finnthorwarth.de>
2026-02-24 21:18:41 +0100

    Content

---
+++ DistributionPackages/Smartgrund.Site/Classes/Domain/Repository/PersonRepository.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Smartgrund\Site\Domain\Repository;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Persistence\Repository;
+use Neos\Party\Domain\Model\Person;
+
+/**
+ * Repository for Person entities.
+ * Required to satisfy Flow's aggregate root consistency rule in the
+ * AbstractParty → Person → User entity hierarchy.
+ *
+ * @Flow\Scope("singleton")
+ */
+class PersonRepository extends Repository
+{
+    const ENTITY_CLASSNAME = Person::class;
+}
